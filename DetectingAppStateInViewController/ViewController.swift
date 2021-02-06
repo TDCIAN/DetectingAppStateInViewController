230e317dc636d14899b7b64b76462cad500e28fd
Jeongmin <tdcian71@gmail.com>
2021-02-06 15:51:28 +0900

    ViewController 설명

--- DetectingAppStateInViewController/ViewController.swift
+++ DetectingAppStateInViewController/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemOrange
+        print("ViewController - viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController - viewWillAppear")
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController - viewWillAppear")
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ViewController - viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController - viewDidDisappear")
+    }
 }
 
